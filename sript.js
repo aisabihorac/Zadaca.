06573ab98ba3acb53d39158d8c85c439d8ee9c7e
aisa <aisabihorac965@gmail.com>
2024-08-07 22:45:38 +0200

    Zadatak 2  - 1 bod

---
+++ sript.js
@@ -0,0 +1,14 @@
+let pozdravi = [
+    "Selam",
+    "Esselamu alejkum",
+    "Dobar dan",
+    "Dobro jutro",
+    "Dobro veče",
+    "Ćao",
+    "Hi",
+    "Heej",
+    "Hello",
+    "Merhaba"
+];
+
+console.log(pozdravi[7]);
